8a4b96def5e2edcd6640537bc8ce5f4f6dc01341
Maryam Chrs <maryam.chrs@gmail.com>
2024-07-29 22:22:37 +0330

    Initial Commit

---
+++ ToeTacTic-VIP-SwiftUI/ToeTacTic_VIP_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToeTacTic_VIP_SwiftUIApp.swift
+//  ToeTacTic-VIP-SwiftUI
+//
+//  Created by Maryam Chrs on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToeTacTic_VIP_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
